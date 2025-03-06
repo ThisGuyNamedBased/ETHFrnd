813d9f2a59b8d5c7a9cdf159cd8d9761307bac4e
Based <GreenBunny0009@gmail.com>
2025-03-06 12:27:57 +0100

    better design I guess

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-document.getElementById('banKeyBtn').addEventListener('click', () => {
-    alert('Ban key functionality triggered.');
-  });
-  document.getElementById('banHWIDBtn').addEventListener('click', () => {
-    alert('Ban HWID functionality triggered.');
+document.getElementById('banSelectedBtn').addEventListener('click', () => {
+    const checkboxes = document.querySelectorAll('.key-checkbox');
+    const selectedKeys = [];
+    checkboxes.forEach(checkbox => {
+      if (checkbox.checked) {
+        selectedKeys.push(checkbox.value);
+      }
+    });
+    if (selectedKeys.length === 0) {
+      alert('No keys selected.');
+    } else {
+      alert('Banning keys: ' + selectedKeys.join(', '));
+    }
   });
   document.getElementById('bulkGenBtn').addEventListener('click', () => {
     var bulkGenModal = new bootstrap.Modal(document.getElementById('bulkGenModal'));
